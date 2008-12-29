b1684a2d0135a0f39fa34e1526a247b050eadb9b
phenxdesign <phenxdesign@be1764a3-6155-0410-8b08-fdea0229669f>
2008-12-29 17:46:38 +0000

    Dev process

---
+++ matrix.js
@@ -0,0 +1,72 @@
+Prototype.Matrix = Class.create({
+  initialize: function() {
+    this.m = this.getIdentity();
+  },
+  getIdentity: function() {
+    return [
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 1]
+    ];
+  },
+  getNull: function() {
+    return [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ];
+  },
+  multiply: function(m) {
+    var result = this.getIdentity();
+
+    for (var x = 0; x < 3; x++) {
+      for (var y = 0; y < 3; y++) {
+        var sum = 0;
+
+        for (var z = 0; z < 3; z++)
+          sum += this.m[x][z] * m[z][y];
+
+        result[x][y] = sum;
+      }
+    }
+    this.m = result;
+    return this;
+  },
+  add: function(m) {
+    var x, y, result = this.getNull();
+
+    for (x = 0; x < 3; x++) {
+      for (y = 0; y < 3; y++) {
+        result[x][y] = this.m[x][y] + m[x][y];
+      }
+    }
+    this.m = result;
+    return this;
+  },
+  rotate: function(angle) {
+    var c = Math.cos(angle), 
+        s = Math.sin(angle),
+        m = [
+      [c,  s, 0],
+      [-s, c, 0],
+      [0,  0, 1]
+    ];
+    return this.multiply(m);
+  },
+  translate: function(x, y) {
+    var m = [
+      [1, 0, 0],
+      [0, 1, 0],
+      [x, y, 1]
+    ];
+    return this.multiply(m);
+  },
+  scale: function(x, y) {
+    var m = [
+      [x, 0, 0],
+      [0, y, 0],
+      [0, 0, 1]
+    ];
+    return this.multiply(m);
+  }
+});
