ebe44b2c1222700102af3e7b446bb763a7c7d84a
phenxdesign <phenxdesign@be1764a3-6155-0410-8b08-fdea0229669f>
2009-05-22 14:35:27 +0000

    Changed the method to apply the fix (http://dean.edwards.name/weblog/2005/06/base64-sexy/)

--- iedatauri/datauri.js
+++ iedatauri/datauri.js
@@ -1,55 +1,46 @@
-function _process_datauri(debug){
-	function _(obj, prop, css) {
-		var d;
-		if (obj[prop] && (d = obj[prop].match(css ? /^url\(data:(.*)\)$/ : /^data:(.*)$/))){
-			d = new String(d[1]).replace(/=/g, '%3D').replace(/\//g, '%2F').replace(/\+/g, '%2B');
-			if (d.length >= 2048) {
-				// POST Ajax Call
-			}
-			else obj[prop] = (css ? 'url('+o.path+'?'+d+')' : o.path+'?'+d);
-			return 1;
-		}
-		return 0;
-	}
-	
-	if (debug) var start = new Date().getTime();
-	
-	var total = 0, i, j, k, ss, tag, list, d = document, o = window._ie_datauri || {},
-	
-	tags = {
-		a: 'href',
-		img: 'src',
-		object: 'data',
-		link: 'href',
-		script: 'src'
-	},
-	styles = ['backgroundImage', 'cursor'];
-	
-	o.path = o.path || 'd.php';
-		
-	// HTML tags
-	for (tag in tags) {
-		list = d.getElementsByTagName(tag);
-		for(i = 0; i < list.length; i++)
-			total += _(list[i], tags[tag]);
-	}
-	
-	// Stylesheets
-	for(i = 0; i < d.styleSheets.length; i++) {
-		ss = d.styleSheets[i];
-		for(j = 0; j < ss.rules.length; j++)
-			for (k = 0; k < styles.length; k++)
-				total += _(ss.rules[j].style, styles[k], true);
-	}
-	
-	// Inline styles
-	var all = document.getElementsByTagName('*'); // document.all
-	for(i = 0; i < all.length; i++)
-		for (j = 0; j < styles.length; j++)
-			total += _(all[i].style, styles[j], true);
-			
-	if (debug) {
-		var time = (new Date().getTime() - start);
-		document.getElementById(debug).innerHTML += time+'ms to process '+total+' data URIs ('+(time/total).toFixed(3)+'ms per data URI)';
-	}
-}
+var _ie_datauri = {
+  options: {
+    debug: null,
+    path: 'd.php'
+  },
+  fixAll: function(options){
+    var o = _ie_datauri.options;
+    o.path = o.path || options.path;
+    o.debug = o.debug || options.debug;
+    
+  	if (o.debug) var start = new Date().getTime();
+  	
+  	var i, j, k, ss, tag, list, d = document,
+  	styles = ['backgroundImage', 'cursor'];
+
+  	// Stylesheets
+  	for(i = 0; i < d.styleSheets.length; i++) {
+  		ss = d.styleSheets[i];
+  		for(j = 0; j < ss.rules.length; j++)
+  			for (k = 0; k < styles.length; k++)
+  				_ie_datauri.fixElement(ss.rules[j].style, styles[k], true);
+  	}
+  	
+  	// Inline styles
+  	var all = document.getElementsByTagName('*'); // document.all
+  	for(i = 0; i < all.length; i++)
+  		for (j = 0; j < styles.length; j++)
+  			_ie_datauri.fixElement(all[i].style, styles[j], true);
+  			
+  	if (o.debug) {
+  		var time = (new Date().getTime() - start);
+  		document.getElementById(o.debug).innerHTML += time+'ms';
+  	}
+  },
+  fixElement: function (obj, prop, css) {
+    if (!css) obj.runtimeStyle.behavior = "none";
+    var d, o = _ie_datauri.options;
+  	if (obj[prop] && (d = obj[prop].match(css ? /^url\(data:(.*)\)$/ : /^data:(.*)$/))){
+  		d = new String(d[1]).replace(/=/g, '%3D').replace(/\//g, '%2F').replace(/\+/g, '%2B');
+  		if (d.length >= 2048) {
+  			// POST Ajax Call
+  		}
+  		else obj[prop] = (css ? 'url('+o.path+'?'+d+')' : o.path+'?'+d);
+  	}
+  }
+};
